78fa04369dd4f745b99cbcc6c2c25b3e15280ed1
Christian Wulf <chw@informatik.uni-kiel.de>
2015-04-28 15:20:21 +0200

    parameterized custom pmd rule MissingPluralVariableName

--- src/main/java/de/chw/MissingPluralVariableName.java
+++ src/main/java/de/chw/MissingPluralVariableName.java
@@ -1,16 +1,64 @@
 package de.chw;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
 
+import net.sourceforge.pmd.lang.java.ast.ASTCompilationUnit;
 import net.sourceforge.pmd.lang.java.ast.ASTFieldDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
 import net.sourceforge.pmd.lang.java.ast.AbstractJavaAccessNode;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
+import net.sourceforge.pmd.lang.rule.properties.StringMultiProperty;
 
 public class MissingPluralVariableName extends AbstractJavaRule {
 
-	@SuppressWarnings("rawtypes")
-	private static final Class<Collection> COLLECTION_TYPE = Collection.class;
+	public static final StringMultiProperty COLLECTION_TYPE_NAMES_DESCRIPTOR = new StringMultiProperty("collectionTypeNames",
+			"Collection type names", new String[] { Collection.class.getName(), Set.class.getName(), List.class.getName() }, 1.0f, ',');
+
+	public static final StringMultiProperty EXCLUDED_COLLECTION_TYPE_NAMES_DESCRIPTOR = new StringMultiProperty("excludedCollectionTypeNames",
+			"Excluded collection type names", new String[] { Queue.class.getName(), Stack.class.getName() }, 2.0f, ',');
+
+	private final Set<Class<?>> collectionTypes = new HashSet<Class<?>>();
+	private final Set<Class<?>> excludedCollectionTypes = new HashSet<Class<?>>();
+
+	public MissingPluralVariableName() {
+		definePropertyDescriptor(COLLECTION_TYPE_NAMES_DESCRIPTOR);
+		definePropertyDescriptor(EXCLUDED_COLLECTION_TYPE_NAMES_DESCRIPTOR);
+
+		addRuleChainVisit(ASTCompilationUnit.class);
+		addRuleChainVisit(ASTFieldDeclaration.class);
+		addRuleChainVisit(ASTLocalVariableDeclaration.class);
+	}
+
+	@Override
+	public Object visit(ASTCompilationUnit node, Object data) {
+		List<String> collectionTypeNames = Arrays.asList(super.getProperty(COLLECTION_TYPE_NAMES_DESCRIPTOR));
+		for (String collectionTypeName : collectionTypeNames) {
+			Class<?> collectionType = resolveClassByName(collectionTypeName);
+			collectionTypes.add(collectionType);
+		}
+
+		List<String> excludedCollectionTypeNames = Arrays.asList(super.getProperty(EXCLUDED_COLLECTION_TYPE_NAMES_DESCRIPTOR));
+		for (String excludedCollectionTypeName : excludedCollectionTypeNames) {
+			Class<?> excludedCollectionType = resolveClassByName(excludedCollectionTypeName);
+			excludedCollectionTypes.add(excludedCollectionType);
+		}
+
+		return data;
+	}
+
+	private Class<?> resolveClassByName(String className) {
+		try {
+			return Class.forName(className);
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException(e);
+		}
+	}
 
 	@Override
 	public Object visit(final ASTFieldDeclaration node, final Object data) {
@@ -18,7 +66,7 @@ public class MissingPluralVariableName extends AbstractJavaRule {
 		Class<?> type = node.getType();
 		addViolationUponMissingPlural(node, data, variableName, type);
 
-		return super.visit(node, data);
+		return data;
 	}
 
 	@Override
@@ -27,13 +75,24 @@ public class MissingPluralVariableName extends AbstractJavaRule {
 		Class<?> type = node.getTypeNode().getType();
 		addViolationUponMissingPlural(node, data, variableName, type);
 
-		return super.visit(node, data);
+		return data;
 	}
 
 	private void addViolationUponMissingPlural(final AbstractJavaAccessNode node, final Object data, final String variableName, final Class<?> type) {
 		if (null != type) {
-			if (COLLECTION_TYPE.isAssignableFrom(type) && !isPlural(variableName)) {
-				addViolation(data, node, new Object[] { variableName, type.getName() });
+			for (Class<?> excludedCollectionType : excludedCollectionTypes) {
+				if (excludedCollectionType.isAssignableFrom(type)) {
+					return;
+				}
+			}
+
+			for (Class<?> collectionType : collectionTypes) {
+				if (collectionType.isAssignableFrom(type)) {
+					if (!isPlural(variableName)) {
+						addViolation(data, node, new Object[] { variableName, type.getName() });
+						break;
+					}
+				}
 			}
 		}
 	}
